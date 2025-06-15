ed4b1b37a9fa1d9e89d34cca17ce08b7504b709a
cesar angulo <cesarangulo@cesars-Mac-Studio.local>
2025-06-14 21:11:50 -0700

    fixing ts-lint for prisma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,29 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Base config
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+
+  // Main ESLint config (base, plugins, etc.)
+  // You can include other base config here
+  {
+    files: ['src/generated/**/*.{ts,tsx,js,jsx,d.ts}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unnecessary-type-constraint': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+    },
+  },
 
 export default eslintConfig;
